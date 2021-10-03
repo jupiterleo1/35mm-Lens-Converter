e85a496272723ee7d7e26369630a367a4bb875d4
jupiterleo1 <info@jupiterleo.com>
2021-10-03 18:18:50 -0400

    1 file modified

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function Container() {
 function Heading() {
   return (
     <div>
-          <h1 className="title">Lens Converter</h1>
+          <h1 className="title">35mm Lens Converter</h1>
     </div>
   );
 }
@@ -25,8 +25,17 @@ function Form() {
   return (
     <div className="forms">
       <form>
-        <label for="fname">Your Lens mm.</label>
+        <input type="radio" id="m43" name="m43" />
+        <label for="m43">Micro Four Thirds</label>
         <br />
+        <input type="radio" id="apsc" name="apsc" />
+        <label for="apsc">APS-C</label>
+        <br />
+        <input type="radio" id="1inch" name="1inch" />
+        <label for="1 inch">1 inch. Sensor</label>
+        <br />
+
+        <label for="fname">Your Lens mm.</label>
         <input type="number" id="fname" name="fname" size="6" maxlength="6" />
         <br />
         <label for="lname">35 mm equivqlent name:</label>
